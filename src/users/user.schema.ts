e683789a3ac5f236ae3056b9b0aa725bc1eeec49
JuanPabloSQ <jp.sherka@gmail.com>
2024-11-19 10:55:19 -0300

    add schema

---
+++ src/users/user.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ collection: 'users' }) 
+export class User extends Document {
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  firstName: string; 
+
+  @Prop({ required: true })
+  lastName: string; 
+
+  @Prop({ required: true })
+  birthDate: Date; 
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
